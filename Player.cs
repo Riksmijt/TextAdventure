a438832cbe5b7bd437ba345a5684a02851208fa4
Rik Smit <rik.smit2912@gmail.com>
2018-06-04 09:16:08 +0100

    initial commit

---
+++ Player.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace ZuulCS
+{
+    public class Player
+    {
+        private Room currentRoom;
+        private int health;
+        
+        public Room CurrentRoom { get{ return currentRoom; } set { currentRoom = value; } }
+
+        public Player()
+        {
+            
+        }
+        public void damage(uint amountDamage)
+        {
+            health = health - (int) amountDamage;
+        }
+        public void heal(int amountHeal) 
+        {
+            health = health + amountHeal;
+        }
+
+        public bool isAlive()
+        {
+            if (health <= 0)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
